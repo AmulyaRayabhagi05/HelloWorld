c6a4420adeeb94c377ecce644ae18b966315e0b8
AmulyaRayabhagi05 <114515586+AmulyaRayabhagi05@users.noreply.github.com>
2023-07-19 23:18:55 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,67 @@
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+       //2a.
+       System.out.println("Hello World!");
+       //3a
+       System.out.print("Hello \nWorld!");
+       //4
+       System.out.print("\nHello \n\nWorld!");
+       //5
+       System.out.println("\n************* \n*** Hello ***\n*** World ***\n*************");
+       //6
+       System.out.print("Hello ");
+       System.out.println("World!");
+       //7
+       System.out.println("###");
+       System.out.println("# #");
+       System.out.println("###");
+       //8
+       System.out.print("Amulya, ");
+       System.out.println("Amulya");
+       System.out.println("");
+       System.out.println("Amulya");
+       //9
+       System.out.print("Something ");
+       System.out.print("Something ");
+       System.out.println("SOMETHING");
+       //10
+       System.out.println("***");
+       System.out.println("*  *");
+       System.out.println("*  *");
+       System.out.println("***");
+       System.out.println("*");
+       System.out.println("*");
+       System.out.println("*");
+       //11
+       // Printing the riddle 
+       int child_number= 5;
+       System.out.println("Mary is the "+ child_number +"th daughter.");
+       // ASCII ART- dog
+       System.out.println("                  | \\_.");
+       System.out.println("                  |     \\___");
+       System.out.println("                  {}   :   /  ");
+       System.out.println("                  /      _/ ");
+       System.out.println("                 ****** ' ");
+       System.out.println("               /      } ");
+       System.out.println("              /     \\ /");
+       System.out.println("          \\ / '    \\ \\ \\");
+       System.out.println("            '\\    / _ \\ \\ ");
+       System.out.println("             '~~~~'   '~~' ");
+       //Advance
+       System.out.println("**********");
+       System.out.println("* \\\\\\/// *");
+       System.out.println("*  \\\\//  *");
+       System.out.println("*   \\/   *");
+       System.out.println("**********");
+       
+       
+       
+       
+       
+        
+       
+       
+    }
+}
